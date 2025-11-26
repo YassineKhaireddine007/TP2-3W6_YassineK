76031f49b03fa8702bbf030b1c71bddf7b5e5261
YassineKhaireddine007 <6327360@cegepmontpetit.ca>
2025-11-25 21:02:20 -0500

    FCT Trainer-Detail

--- TP2_JuliePro_JS_ViewModelService_DepartA25-f37af93f4b269887e0f1966343c92014/TP2_JuliePro_JS_ViewModelService-master/JuliePro/wwwroot/js/site.js
+++ TP2_JuliePro_JS_ViewModelService_DepartA25-f37af93f4b269887e0f1966343c92014/TP2_JuliePro_JS_ViewModelService-master/JuliePro/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+
+    
+    $('.trainer-card').on('mouseenter', function () {
+        $(this).find('.trainer-details').addClass('show');
+    });
+
+    
+    $('.trainer-card').on('mouseleave', function () {
+        $(this).find('.trainer-details').removeClass('show');
+    });
+
+});
